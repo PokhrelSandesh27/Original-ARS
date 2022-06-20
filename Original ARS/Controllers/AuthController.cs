992563be55c46c42178633cc1ce801ff7253a548
PokhrelSandesh27 <sandeshpokhrel17@gmail.com>
2022-06-20 18:30:15 +0545

    updated first commit

---
+++ Original ARS/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Original_ARS.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ViewResult MainPage()
+        {
+            return View();
+
+        }
+        public ViewResult Login()
+        {
+            return View();
+
+        }
+        public ViewResult Create()
+        {
+            return View();
+
+        }
+        public ViewResult Display()
+        {
+            return View();
+
+        }
+    }
+}
